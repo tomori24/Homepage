6bd72f7b555b76a63e5a597684425b8f4804d0cd
tomori <nt24101310@gmail.com>
2020-08-04 02:23:40 +0900

    作者情報を変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `tomori HomePage`,
+    description: `音楽活動用サイト`,
+    author: `tomori`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
