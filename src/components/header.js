a996ec8f0265734351d1770ab71606d39136888e
tomori <nt24101310@gmail.com>
2020-08-04 21:24:49 +0900

    ヘッダとフッタのCSSをスタイルシートに記述

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,7 @@
 import React from "react"
 
 const Header = () => (
-  <header
-    style={{
-      background: `white`,
-      width: `100%`,
-      height: `50px`
-    }}
-  >
-  </header>
+  <header />
 )
 
 export default Header
