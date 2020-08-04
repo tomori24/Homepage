3ee2bb69502ce68f3b767d566752976bda6ea7ae
tomori <nt24101310@gmail.com>
2020-08-04 21:24:14 +0900

    Contactsコンポーネントを作成

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -2,43 +2,37 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faTwitter, faYoutube, faSoundcloud } from "@fortawesome/free-brands-svg-icons";
 
-const Footer = () => (
-  <footer 
-    style={{
-      background: `white`,
-      marginTop: `2em`,
-      marginBottom: `2em`
-    }}
-  >
+const Contacts = () => (
+  <>
     <a style={{ boxShadow: "none", }} href="https://twitter.com/tmri24">
       <FontAwesomeIcon
-        style={{ height: "2em", width: "2em", margin: "auto", }}
+        style={{ height: "2.5em", width: "2.5em", margin: "1em", }}
         color="#3eaded"
         icon={faTwitter}
       />
     </a>
     <a style={{ boxShadow: "none", }} href="https://www.instagram.com/tomori24">
       <FontAwesomeIcon
-        style={{ height: "2em", width: "2em", margin: "auto", }}
+        style={{ height: "2.5em", width: "2.5em", margin: "1em", }}
         color="#3eaded"
         icon={faInstagram}
       />
     </a>
     <a style={{ boxShadow: "none", }} href="https://www.youtube.com/channel/UCsVYnVMynZqS6V7D69LM2Zg">
       <FontAwesomeIcon
-        style={{ height: "2em", width: "2em", margin: "auto", }}
+        style={{ height: "2.5em", width: "2.5em", margin: "1em", }}
         color="#3eaded"
         icon={faYoutube}
       />
     </a>
     <a style={{ boxShadow: "none", }} href="https://soundcloud.com/tomori1039">
       <FontAwesomeIcon
-        style={{ height: "2em", width: "2em", margin: "auto", }}
+        style={{ height: "2.5em", width: "2.5em", margin: "1em", }}
         color="#3eaded"
         icon={faSoundcloud}
       />
     </a>
-  </footer>
+  </>
 )
 
-export default Footer
+export default Contacts
